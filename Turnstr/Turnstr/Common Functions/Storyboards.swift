3eca68ff81ecf77ada4b389ea81374819184e2cb
Kamal <kkbhardwaj20@gmail.com>
2017-07-13 00:18:12 +0530

    Tabbar added

---
+++ Turnstr/Turnstr/Common Functions/Storyboards.swift
@@ -0,0 +1,36 @@
+
+//
+//  Storyboards.swift
+//  Turnstr
+//
+//  Created by Kamal on 12/07/17.
+//  Copyright © 2017 Ankit Saini. All rights reserved.
+//
+
+import Foundation
+
+
+enum Storyboards: String {
+    case profileStoryboard = "Profile"
+    case chatStoryboard = "Chat"
+    case liveStoryboard = "LiveFeed"
+    case photoStoryboard = "PhotoAlbum"
+    case storyStoryboard = "Stories"
+    
+    func initialVC() -> UIViewController? {
+        let storyboard = UIStoryboard(name: self.rawValue, bundle: nil)
+        let initialVC = storyboard.instantiateInitialViewController()
+        return initialVC
+    }
+    
+    func initialVC(with storyboardId: StoryboardIds) -> UIViewController? {
+        let storyboard = UIStoryboard(name: self.rawValue, bundle: nil)
+        let initialVC = storyboard.instantiateViewController(withIdentifier: storyboardId.rawValue)
+        return initialVC
+    }
+}
+
+enum StoryboardIds: String {
+    
+    case example = "Example"
+}
---
+++ Turnstr/Turnstr/TabbarController.swift
@@ -0,0 +1,101 @@
+//
+//  TabbarController.swift
+//  Turnstr
+//
+//  Created by Kamal on 12/07/17.
+//  Copyright © 2017 Ankit Saini. All rights reserved.
+//
+
+import UIKit
+
+class TabbarController: UIViewController {
+
+    @IBOutlet var contentView: UIView?
+    @IBOutlet weak var btnLiveFeed: UIButton?
+    @IBOutlet weak var btnProfile: UIButton?
+    @IBOutlet weak var btnStory: UIButton?
+    @IBOutlet weak var btnChat: UIButton?
+    @IBOutlet weak var btnPhotoAlbum: UIButton?
+    
+    var feeds: UIViewController?
+    var profile: UIViewController?
+    var story: UIViewController?
+    var chat: UIViewController?
+    var photos: UIViewController?
+    
+    
+    fileprivate var activeViewController: UIViewController? {
+        didSet {
+            removeInactiveViewController(oldValue)
+            updateActiveViewController()
+        }
+    }
+    
+    fileprivate func updateActiveViewController() {
+        if let activeVC = activeViewController {
+            addChildViewController(activeVC)
+            
+            activeVC.view.frame = contentView?.bounds ?? .zero
+            contentView?.addSubview(activeVC.view)
+            
+            activeVC.didMove(toParentViewController: self)
+        }
+    }
+    
+    fileprivate func removeInactiveViewController(_ inactiveViewController: UIViewController?) {
+        if let inActiveVC = inactiveViewController {
+            inActiveVC.willMove(toParentViewController: nil)
+            
+            inActiveVC.view.removeFromSuperview()
+            
+            inActiveVC.removeFromParentViewController()
+        }
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSelectAction(_ sender: UIButton) {
+        
+        if sender === btnLiveFeed {
+            if feeds == nil {
+                feeds = Storyboards.liveStoryboard.initialVC()
+            }
+            self.activeViewController = feeds
+        }
+        
+        if sender === btnProfile {
+            if profile == nil {
+                profile = Storyboards.profileStoryboard.initialVC()
+            }
+            self.activeViewController = profile
+        }
+        
+        if sender === btnStory {
+            if story == nil {
+                story = Storyboards.storyStoryboard.initialVC()
+            }
+            self.activeViewController = story
+        }
+        
+        if sender === btnChat {
+            if chat == nil {
+                chat = Storyboards.chatStoryboard.initialVC()
+            }
+            self.activeViewController = chat
+        }
+        
+        if sender === btnPhotoAlbum {
+            if photos == nil {
+                photos = Storyboards.photoStoryboard.initialVC()
+            }
+            self.activeViewController = photos
+        }
+    }
+    
+
+}
